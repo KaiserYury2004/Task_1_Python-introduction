e5179a2b7da90631b288ea1d3fda20b10713a292
KaiserYury2004 <129221692+KaiserYury2004@users.noreply.github.com>
2024-05-31 11:49:06 +0300

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -5,8 +5,3 @@ INNER JOIN students s ON r.id = s.room
 GROUP BY r.name
 ORDER BY Age_diff DESC
 LIMIT 5;
-#Для MSSMS
-SELECT TOP 5 r.name, MAX(DATEDIFF(YEAR, s.birthday, GETDATE())) - MIN(DATEDIFF(YEAR, s.birthday, GETDATE())) AS Age_diff 
-FROM rooms r INNER JOIN students s ON r.id = s.room 
-GROUP BY r.name 
-ORDER BY Age_diff DESC 
